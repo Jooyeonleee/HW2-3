e4ae7dc2959f3cd2413fc03533908a02345db6cc
cmomente <jan201701@gmail.com>
2020-12-14 17:07:02 +0900

    add 1714376 part of GCD

--- MainProgram.java
+++ MainProgram.java
@@ -90,6 +90,9 @@ public class MainProgram {
 	}
 	
 	public int calGCD(int a, int b) {
+		if(a < 0) a = a * (-1);
+		if(b < 0) b = b * (-1);
+		
 		if (a>=b) {
 			if(b == 0) return a;
 			return calGCD(b, a % b);
